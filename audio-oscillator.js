c9ca7483e0b361be85172bb6b68f5f1f888baf18
Koji Yokokawa <koji.yokokawa@gmail.com>
2015-05-11 19:47:05 +0900

    added blocks for oscillator start/stop.

--- audio-oscillator.js
+++ audio-oscillator.js
@@ -4,26 +4,53 @@
  * yokobond <koji.yokokawa@yengawa.com>
  */
 
-(function(ext) {
+(function (ext) {
+    var audioctx;
+    if (typeof (webkitAudioContext) !== "undefined") {
+        audioctx = new webkitAudioContext();
+    }
+    else if (typeof (AudioContext) !== "undefined") {
+        audioctx = new AudioContext();
+    }
+
+    var osc = audioctx.createOscillator();
+    var gain = audioctx.createGain();
+    osc.connect(gain);
+    gain.connect(audioctx.destination);
+    osc.type = "sine";
+    osc.frequency.value = 440;
+    gain.gain.value = 0.5;
+    
     // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
+    ext._shutdown = function () {
+        osc.stop(0);
+    };
 
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
+    ext._getStatus = function () {
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.my_first_block = function() {
-        // Code that gets executed when the block is run
+    ext.oscillatorType = function (type) {
+        osc.type = type;
+    }
+
+    ext.oscillatorStart = function () {
+        osc.start(0);
+    };
+    
+    ext.oscillatorStop = function () {
+        osc.stop(0);
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name
-            [' ', 'my first block', 'my_first_block'],
+            [' ', 'start oscillator', 'oscillatorStart'],
+            [' ', 'stop oscillator', 'oscillatorStop'],
         ]
     };
 
